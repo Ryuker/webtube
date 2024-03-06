5016cbadc33950f6dc0814a1645688edb12c27fc
ryuker <lifeisagamenotaproduct@gmail.com>
2024-03-06 14:17:44 +0100

    some cleanup

--- webtube/src/components/CategoryPills.tsx
+++ webtube/src/components/CategoryPills.tsx
@@ -24,18 +24,25 @@ export default function CategoryPills({ categories, selectedCategory, onSelect }
           </Button>
         ))}
       </div>
-      {isLeftVisible && <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
-        <Button variant="ghost" size="icon" 
-          className="h-full aspect-square w-auto p-1.5">
-          <ChevronLeft />
-        </Button>
-      </div>}
-      {isRightVisible && <div className="absolute flex justify-end right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full">
-        <Button variant="ghost" size="icon" 
-          className="h-full aspect-square w-auto p-1.5">
-          <ChevronRight />
-        </Button>
-      </div>}
+
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button variant="ghost" size="icon" 
+            className="h-full aspect-square w-auto p-1.5">
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+
+      {isRightVisible && (
+        <div className="absolute flex justify-end right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full">
+          <Button variant="ghost" size="icon" 
+            className="h-full aspect-square w-auto p-1.5">
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
+      
     </div>
   );
 }
