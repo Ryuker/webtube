aa6559fa80625ad92298ac001822d6a9d02a1004
ryuker <lifeisagamenotaproduct@gmail.com>
2024-03-07 13:17:39 +0100

    renamed sidebar, added SmallSideBarItem component in the file, fixed container styling of grid

---
+++ webtube/src/layouts/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { Home } from "lucide-react";
+import { ElementType } from "react";
+
+export default function Sidebar(){
+  return(
+    <>
+      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+        <SmallSidebarItem Icon={Home} title="Home" url="/" />
+      </aside>
+    </>
+  );
+}
+
+type SmallSidebarItemProps = {
+  Icon: ElementType
+  title: string
+  url: string
+}
+
+function SmallSidebarItem({Icon, title, url}: SmallSidebarItemProps){
+  return (
+    <a href={url} className="">
+      <Icon className="w-6 h-6" />
+      <div className="text-sm">{title}</div>
+    </a>
+  );
+}
