8dd84bb5a16940daa21e78932be51534e06b0c0f
ryuker <lifeisagamenotaproduct@gmail.com>
2024-03-07 12:48:49 +0100

    changed SideBar to Sidebar

--- webtube/src/App.tsx
+++ webtube/src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
       <div className="max-h-screen flex flex-col">
         <PageHeader />
         <div className="grid grid-cols-[auto, 1fr] flex-grow-1 overflow-auto">
-          <SideBar />
+          <Sidebar />
           <div className="overflow-x-hidden px-8 pb-4">
             <div className="sticky top-0 bg-white z-10 pb-4">
               <CategoryPills 
--- webtube/src/components/SideBar.tsx
+++ webtube/src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-export default function SideBar(){
+export default function Sidebar(){
   return(
     <div>Side Bar</div>
   );
