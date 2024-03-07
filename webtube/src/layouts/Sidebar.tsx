2a99a889d10dae4694451342fbec84edf751b323
ryuker <lifeisagamenotaproduct@gmail.com>
2024-03-07 13:27:35 +0100

    added icons to sidebar, fixed ghost hover styling in button variants

--- webtube/src/layouts/Sidebar.tsx
+++ webtube/src/layouts/Sidebar.tsx
@@ -1,11 +1,16 @@
-import { Home } from "lucide-react";
+import { Clapperboard, Home, Library, Repeat } from "lucide-react";
 import { ElementType } from "react";
+import { buttonStyles } from "../components/Button";
+import { twMerge } from "tailwind-merge";
 
 export default function Sidebar(){
   return(
     <>
       <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
         <SmallSidebarItem Icon={Home} title="Home" url="/" />
+        <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+        <SmallSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
+        <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
     </>
   );
@@ -19,7 +24,10 @@ type SmallSidebarItemProps = {
 
 function SmallSidebarItem({Icon, title, url}: SmallSidebarItemProps){
   return (
-    <a href={url} className="">
+    <a href={url} 
+      className={twMerge(buttonStyles({ variant: "ghost"}),
+      "py-4 px-1 flex flex-col items-center rounded-lg gap-1")}
+    >
       <Icon className="w-6 h-6" />
       <div className="text-sm">{title}</div>
     </a>
